4a027ac47fd8fc852e30d549834c935234a4fa8a
LaiLeeHan <allen@profetai.com>
2023-12-25 05:47:09 +0000

    chore: pass if not dir

--- test_all_solution.py
+++ test_all_solution.py
@@ -1,10 +1,13 @@
 import os
 import subprocess
+from pathlib import Path
 
 curr = os.getcwd()
 
 for _dir in os.listdir():
     if _dir.startswith((".", "_")):
+        continue
+    if not (Path(curr) / _dir).is_dir():
         continue
     os.chdir(curr)
     print(_dir)
