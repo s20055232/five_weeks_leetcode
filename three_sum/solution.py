7d51b73ba01041c71fa5ea6aec7e5ee7f44275e9
LaiLeeHan <allen@profetai.com>
2023-12-25 13:52:59 +0000

    new: add three sum

--- three_sum/solution.py
+++ three_sum/solution.py
@@ -1,18 +1,24 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        memo = []
+        result = []
         nums = sorted(nums)
-        for pt in range(len(nums)):
-            pointer = pt + 1
-            result = self.twoSum(nums[pointer:], 0 - nums[pt])
-            if result:
-                print(pointer)
-                [first, second] = result
-                print(first)
-                print(second)
-                memo.append([nums[pt], nums[first + pointer], nums[second + pointer]])
-        return memo
 
+        for idx, val in enumerate(nums):
+            if idx > 0 and nums[idx - 1] == val:
+                continue
 
-a = sorted([-1, 0, 1, 2, -1, -4])
-Solution().threeSum(a)
+            left, right = idx + 1, len(nums) - 1
+            while left < right:
+                total = val + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    result.append([val, nums[left], nums[right]])
+                    left += 1
+                    # add number until not repeat
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+
+        return result
--- three_sum/test_solution.py
+++ three_sum/test_solution.py
@@ -11,3 +11,8 @@ def test_solution_2(solution):
 def test_solution3(solution):
     nums = [0, 0, 0]
     assert solution.threeSum(nums) == [[0, 0, 0]]
+
+
+def test_solution4(solution):
+    nums = [-1, 0, 1, 0]
+    assert solution.threeSum(nums) == [[-1, 0, 1]]
